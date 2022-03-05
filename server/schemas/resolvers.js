b14ba7c2e1af835cf28f75a40960f74a3d2a9954
ening <ening6649@yahoo.com>
2022-03-05 13:27:20 -0500

    schemas initial setup

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,8 +1,107 @@
 const { AuthenticationError } = require('apollo-server-express');
-
+const { User, Coupon } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
-}
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id })
+          .select('-__v -password')
+          .populate('coupons')
+          .populate('comments');
+
+        return userData;
+      }
+
+      throw new AuthenticationError('Not logged in');
+    },
+    users: async () => {
+      return User.find()
+        .select('-__v -password')
+        .populate('coupons')
+        .populate('comments');
+    },
+    user: async (parent, { username }) => {
+      return User.findOne({ username })
+        .select('-__v -password')
+        .populate('coupons')
+        .populate('comments');
+    },
+    coupons: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Thought.find(params).sort({ createdAt: -1 });
+    },
+    coupon: async (parent, { _id }) => {
+      return Thought.findOne({ _id });
+    }
+  },
+
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    addCoupon: async (parent, args, context) => {
+      if (context.user) {
+        const coupon= await coupon.create({ ...args, username: context.user.username });
+
+        await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { coupons: coupon._id } },
+          { new: true }
+        );
+
+        return coupon;
+      }
+
+      throw new AuthenticationError('You need to be logged in!');
+    },
+    addComment: async (parent, { thoughtId, reactionBody }, context) => {
+      if (context.user) {
+        const updatedCoupon = await Coupon.findOneAndUpdate(
+          { _id: couponId },
+          { $push: { comments: { commentBody, username: context.user.username } } },
+          { new: true, runValidators: true }
+        );
+
+        return updatedCoupon;
+      }
+
+      throw new AuthenticationError('You need to be logged in!');
+    },
+    addComment: async (parent, { friendId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { comments: commentId } },
+          { new: true }
+        ).populate('comments');
+
+        return updatedUser;
+      }
+
+      throw new AuthenticationError('You need to be logged in!');
+    }
+  }
+};
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,51 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    commentCount: Int
+    coupons: [Coupon]
+    comments: [User]
+  }
 
+  type Coupon {
+    _id: ID
+    couponText: String
+    createdAt: String
+    username: String
+    commentCount: Int
+    comments: [Comment]
+  }
+
+  type comment {
+    _id: ID
+    commentBody: String
+    createdAt: String
+    username: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+    coupons(username: String): [Coupon]
+    coupons(_id: ID!): Coupon
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addCoupon(couponText: String!): Coupon
+    addComment(couponId: ID!, commentBody: String!): Coupon
+    addComment(commentId: ID!): User
+  }
 `;
 
 module.exports = typeDefs;
