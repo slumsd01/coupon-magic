2c028820b143cce944021ef5fc4346d6ded0d951
mkanczuzewski <mkanczuzewski@gmail.com>
2022-03-07 16:30:25 -0500

    completed profile mockup of content/bootstrap/css

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -4,9 +4,34 @@ import Dropdown from 'react-bootstrap/Dropdown';
 import ButtonGroup from "react-bootstrap/ButtonGroup";
 
 const Profile = () => {
-  
+  // const [formState, setFormState] = useState({ email: '', password: '' });
+  // const [login, { error }] = useMutation(LOGIN_USER);
+  // // update state based on form input changes
+  // const handleChange = (event) => {
+  //   const { name, value } = event.target;
 
-  const loggedIn = Auth.loggedIn();
+  //   setFormState({
+  //     ...formState,
+  //     [name]: value,
+  //   });
+  // };
+
+  // // submit form
+  // const handleFormSubmit = async event => {
+  //   event.preventDefault();
+
+  //   try {
+  //     const { data } = await login({
+  //       variables: { ...formState }
+  //     });
+    
+  //     Auth.login(data.login.token);
+  //   } catch (e) {
+  //     console.error(e);
+  //   }
+  // };
+
+  // const loggedIn = Auth.loggedIn();
 
   return (
     <main>
@@ -17,27 +42,52 @@ const Profile = () => {
           </div>
         </div>
       </div>
-      <div className="container bg-secondary-color justify-content-center p-4 mt-4">
-        <div className="d-flex bd-highlight justify-content-center p-4">
-          <div className="bd-highlight col-3 bg-primary-color">
-            My Coupons
+      <form>
+        <div className="container bg-secondary-color justify-content-center p-4 mt-4">
+          <div className="d-flex bd-highlight justify-content-center p-4">
+            <div className="bd-highlight col-3 bg-primary-color">
+              My Coupons
+            </div>
+            <Dropdown as={ButtonGroup}>
+              <Dropdown.Toggle variant="light">Select previously created coupon here!</Dropdown.Toggle>
+              <Dropdown.Menu className="dropdownbutton">
+                <Dropdown.Item eventKey="1">baz</Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Item eventKey="2">foo</Dropdown.Item>
+                <Dropdown.Item eventKey="3">bar</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
           </div>
-          <Dropdown as={ButtonGroup}>
-            <Dropdown.Toggle variant="light">Select previously created coupon here!</Dropdown.Toggle>
-            <Dropdown.Menu className="dropdownbutton">
-              <Dropdown.Item eventKey="1">baz</Dropdown.Item>
-              <Dropdown.Divider />
-              <Dropdown.Item eventKey="2">foo</Dropdown.Item>
-              <Dropdown.Item eventKey="3">bar</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
+          <div className="d-flex col-12 bd-highlight justify-content-center">
+            <button className="d-flex bd-highlight justify-content-center col-2">
+              Submit
+            </button>
+          </div>
         </div>
-        <div className="d-flex col-12 bd-highlight justify-content-center">
-          <button className="d-flex bd-highlight justify-content-center col-2">
-            Submit
-          </button>
+      </form>
+      <form>
+        <div className="container bg-secondary-color justify-content-center p-4 mt-4">
+          <div className="d-flex bd-highlight justify-content-center p-4">
+            <div className="bd-highlight col-3 bg-primary-color">
+              Coupon Search by ID
+            </div>
+            <input
+                className='form-input'
+                placeholder='Insert ID'
+                name='coupon'
+                type='coupon'
+                id='coupon'
+                // value={formState.password}
+                // onChange={handleChange}
+              />
+          </div>
+          <div className="d-flex col-12 bd-highlight justify-content-center">
+            <button className="d-flex bd-highlight justify-content-center col-2">
+              Submit
+            </button>
+          </div>
         </div>
-      </div>
+      </form>
     </main>
   );
 };
