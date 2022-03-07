08ed831281c26f5dc751cff8bef328e130dc52e6
alotfey <me@alotfey.com>
2022-03-07 13:23:28 -0500

    add models schema

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,8 +1,15 @@
 const { AuthenticationError } = require('apollo-server-express');
+const { User,Comment } = require('../models');
 
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
-}
+    Query: {
+      users: async () => {
+        return User.find().sort({ createdAt: -1 });
+      }
+    }
+  };
+  
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,21 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-
+  type User {
+    _id: ID
+    username: String
+    email: String
+    couponId: [Coupon]
+  }
+  type Coupon {
+    _id: ID
+    commentText: String
+    createdAt: String
+    userId: [User]
+  }
+  type Query {
+    users: [User]
+  }
 `;
 
 module.exports = typeDefs;
