3197cb95244e0d5bc3e2578631396eaac131e85e
mkanczuzewski <mkanczuzewski@gmail.com>
2022-03-07 13:17:06 -0500

    fix nomatch

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NoMatch = () => {
   return (
-    <div>
+    <div className='d-flex justify-content-center'>
       Oops, we couldn't find that page.
     </div>
   );
