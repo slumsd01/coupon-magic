dbc323bad360a9e62398e96294c81f0a83697b2d
mkanczuzewski <mkanczuzewski@gmail.com>
2022-03-08 16:40:50 -0500

    Finish up CSS layout

--- client/src/pages/CouponDisplay.js
+++ client/src/pages/CouponDisplay.js
@@ -21,11 +21,11 @@ const CouponDisplay = () => {
 
   return (
     <main>
-      <Container className="d-flex justify-content-between col-6 p-2 mt-4">
+      <Container className="d-flex justify-content-between col-12 p-2 mt-4">
         <Col className="d-flex bd-highlight bg-secondary-color justify-content-center p-4 col-6">
           <div className="d-flex justify-content-left">
             {/* {loggedIn && ( */}
-              <div className="col-12 mb-3">
+              <div className="col-12">
                 <Coupon />
               {/* </div>
             )} */}
@@ -33,23 +33,23 @@ const CouponDisplay = () => {
           </div>  
         </Col>
         <Col className="d-flex bd-highlight justify-content-center p-4 col-6">
-        <div className="flex-row justify-content-right">
-          {/* {loggedIn && ( */}
-            <div className="col-12 mb-3">
-              <ThoughtForm />
+          <div className="flex-row col-12 justify-content-right">
+            {/* {loggedIn && ( */}
+              <div className="col-12">
+                <ThoughtForm />
+              </div>
+            {/* )} */}
+            <div className={`flex-row col-12 ${loggedIn && 'col-lg-12'}`}>
+              {loading ? (
+                <div>Loading...</div>
+              ) : (
+                <ThoughtList
+                  thoughts={thoughts}
+                  title="Some Feed for Thought(s)..."
+                />
+              )}
             </div>
-          {/* )} */}
-          <div className={`flex-row col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
-            {loading ? (
-              <div>Loading...</div>
-            ) : (
-              <ThoughtList
-                thoughts={thoughts}
-                title="Some Feed for Thought(s)..."
-              />
-            )}
           </div>
-        </div>
         </Col>
       </Container>
     </main>
