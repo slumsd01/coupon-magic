b8316a72bc18c0ffbb864e3c560259392765a0c6
slumsd01 <90315740+slumsd01@users.noreply.github.com>
2022-03-08 11:10:40 -0500

    created coupon entry page skeleton

--- client/src/pages/CouponEntry.js
+++ client/src/pages/CouponEntry.js
@@ -9,9 +9,35 @@ const CouponEntry = () => {
 
   return (
     <main>
-      <div>
-        
-      </div>
+        <h2>Create your business's own COUPON MAGIC today!</h2>
+        <form>
+          <label>
+            Name: <br/>
+            <input type="text"/>
+          </label><br/>
+          <label>
+            Product: <br/>
+            <input type="text"/>
+          </label><br/>
+          <label>
+            Vendor / Manufacturer: <br/>
+            <input type="text"/>
+          </label><br/>
+          <label>
+            Amount Off (% or $): <br/>
+            <input type="text"/>
+          </label><br/>
+          <label>
+            Currency: <br/>
+            <input type="text"/>
+          </label><br/>
+          <label>
+            Expiration Date: <br/>
+            <input type="text"/>
+          </label><br/>
+          <button>Submit</button>
+        </form>
+        <h4>Sign in or create an account to be taken to the coupon creation screen.</h4>
     </main>
   );
 };
