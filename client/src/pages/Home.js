7b84fa6cbe7880013434d17680484af8406514cf
slumsd01 <90315740+slumsd01@users.noreply.github.com>
2022-03-08 10:38:35 -0500

    rough layout of home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,15 +3,45 @@ import Auth from '../utils/auth';
 
 
 const Home = () => {
-  
 
   const loggedIn = Auth.loggedIn();
 
   return (
     <main>
-      <div className="flex-row justify-space-between">
-        
-      </div>
+        <h2>Create your business's own COUPON MAGIC today!</h2>
+        <section>
+          <h4>Sign In</h4>
+          <form>
+            <label>
+              Email: <br/>
+              <input type="text"/>
+            </label><br/>
+            <label>
+              Password: <br/>
+              <input type="text"/>
+            </label><br/>
+            <button>Sign In</button>
+          </form>
+        </section>
+        <section>
+          <h4>Create Account</h4>
+          <form>
+            <label>
+              Username: <br/>
+              <input type="text"/>
+            </label><br/>
+            <label>
+              Email: <br/>
+              <input type="text"/>
+            </label><br/>
+            <label>
+              Password: <br/>
+              <input type="text"/>
+            </label><br/>
+            <button>Create Account</button>
+          </form>
+        </section>
+        <h4>Sign in or create an account to be taken to the coupon creation screen.</h4>
     </main>
   );
 };
