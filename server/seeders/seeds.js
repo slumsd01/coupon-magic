ab6f40ed9056291061736cac1d700a9a1b2dda1b
Xiaozhou Ning <firefly7335@Xiaozhous-MacBook.local>
2022-03-10 22:41:51 +0100

    added seeder

--- server/seeders/seeds.js
+++ server/seeders/seeds.js
@@ -1,21 +1,62 @@
+
 // generate dummy data
 const faker = require('faker');
 
 const db = require('../config/connection');
-
+const { Coupon, User, Comment } = require('../models');
 
 db.once('open', async () => {
-  await Thought.deleteMany({});
-  await User.deleteMany({});
+  // await Coupon.deleteMany({});
+  // await User.deleteMany({});
+  // await Comment.deleteMany({})
 
   // create user data
   const userData = [];
 
+  for (let i = 0; i < 25; i += 1) {
+    const username = faker.internet.userName();
+    const email = faker.internet.email(username);
+    const password = faker.internet.password();
 
+    userData.push({ username, email, password });
+  }
 
+  const createdUsers = await User.collection.insertMany(userData);
 
+  // create coupon
+  let createdCoupons = [];
+  for (let i = 0; i < 50; i += 1) {
+    const product = faker.lorem.words(Math.round(Math.random() * 20) + 1);
 
+    const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
+    const { username, _id: userId } = createdUsers.ops[randomUserIndex];
 
+    const createdCoupon = await Coupon.create({ username, product});
+
+    const updatedUser = await User.updateOne(
+      { _id: userId },
+      { $push: { coupons: createdCoupon._id } }
+    );
+
+    createdCoupons.push(createdCoupon);
+  }
+
+  // create comments
+  for (let i = 0; i < 50; i += 1) {
+    const commentText = faker.lorem.words(Math.round(Math.random() * 20) + 1);
+
+    const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
+    const { username } = createdUsers.ops[randomUserIndex];
+
+    const randomCouponIndex = Math.floor(Math.random() * createdCoupons.length);
+    const { _id: couponId } = createdCoupons[randomCouponIndex];
+
+    await Thought.updateOne(
+      { _id: couponId },
+      { $push: { comments: { commentText, username } } },
+      { runValidators: true }
+    );
+  }
 
   console.log('all done!');
   process.exit(0);
