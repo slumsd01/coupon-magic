5d4689b76f862274c66c4e641562872288e6693c
mkanczuzewski <mkanczuzewski@gmail.com>
2022-03-10 16:45:45 -0500

    move login and signup to their own components and started authentication logic

---
+++ client/src/components/Signup.js
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
+
+const Signup = () => {
+  const [formState, setFormState] = useState({ username: '', email: '', password: '' });
+  const [addUser, { error }] = useMutation(ADD_USER);
+
+  // update state based on form input changes
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  // submit form (notice the async!)
+  const handleFormSubmit = async event => {
+    event.preventDefault();
+
+    // use try/catch instead of promises to handle errors
+    try {
+      const { data } = await addUser({
+        variables: { ...formState }
+      });
+    
+      Auth.login(data.addUser.token);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+
+  return (
+    <main className='page'>
+        <h2 className='purple-bg'>Create your business's own COUPON MAGIC today!</h2>
+        <h4 className='left-align bold-text'>Create Account</h4>
+        <section className='green-bg form-section'>
+          <form onSubmit={handleFormSubmit} className='form'>
+            <label className='blue-bg row'>
+            <p className='col-3'>Username:</p>
+              <input 
+              className='col-9'
+              name='username'
+              type='username'
+              id='username'
+              value={formState.username}
+              onChange={handleChange}
+              />
+            </label><br/>
+
+            <label className='blue-bg row'>
+            <p className='col-3'>Email:</p>
+              <input 
+              className='col-9'
+              name='email'
+              type='email'
+              id='email'
+              value={formState.email}
+              onChange={handleChange}
+              />
+            </label><br/>
+
+            <label className='blue-bg row'>
+            <p className='col-3'>Password:</p>
+              <input 
+              className='col-9'
+              name='password'
+              type='password'
+              id='password'
+              value={formState.password}
+              onChange={handleChange}
+              />
+            </label><br/>
+
+            <button className='purple-bg' type='submit'>Create Account</button>
+          </form>
+        </section>
+        {error && <div>Sign up failed</div>}
+        <h4 className='purple-bg'>Sign in or create an account to be taken to the coupon creation screen.</h4>
+    </main>
+  );
+};
+
+export default Signup;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,49 +1,28 @@
 import React from 'react';
+import Login from '../components/Login';
+import Signup from '../components/Signup';
 import Auth from '../utils/auth';
 
+import { Container, Col } from 'react-bootstrap';
+import { useQuery } from '@apollo/client';
+import { QUERY_THOUGHTS, QUERY_ME_BASIC } from '../utils/queries';
 
 const Home = () => {
 
-  const loggedIn = Auth.loggedIn();
-
   return (
-    <main className='page'>
-        <h2 className='purple-bg'>Create your business's own COUPON MAGIC today!</h2>
-        <h4 className='left-align bold-text'>Sign In</h4>
-        <section className='green-bg form-section'>          
-          <form className='form'>
-            <label className='blue-bg row'>
-              <p className='col-3'>Email:</p>
-              <input type="text" className='col-9'/>
-            </label><br/>
-            <label className='blue-bg row'>
-            <p className='col-3'>Password:</p>
-              <input type="text" className='col-9'/>
-            </label><br/>
-            <button className='purple-bg'>Sign In</button>
-          </form>
-        </section>
-        <h4 className='left-align bold-text'>Create Account</h4>
-        <section className='green-bg form-section'>
-          <form className='form'>
-            <label className='blue-bg row'>
-            <p className='col-3'>Username:</p>
-              <input type="text" className='col-9'/>
-            </label><br/>
-            <label className='blue-bg row'>
-            <p className='col-3'>Email:</p>
-              <input type="text" className='col-9'/>
-            </label><br/>
-            <label className='blue-bg row'>
-            <p className='col-3'>Password:</p>
-              <input type="text" className='col-9'/>
-            </label><br/>
-            <button className='purple-bg'>Create Account</button>
-          </form>
-        </section>
-        <h4 className='purple-bg'>Sign in or create an account to be taken to the coupon creation screen.</h4>
+    <main>
+      <Container>
+        <div>
+          <Login />
+        </div>
+        <div>
+          <Signup />
+        </div>
+      </Container>
     </main>
   );
 };
+
+
 
 export default Home;
