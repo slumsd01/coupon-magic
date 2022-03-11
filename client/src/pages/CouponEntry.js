eb607408fdd2c3eabf7e5426f90c3b76b83f9bc2
slumsd01 <90315740+slumsd01@users.noreply.github.com>
2022-03-11 15:09:51 -0500

    matched form data to server definitions

--- client/src/pages/CouponEntry.js
+++ client/src/pages/CouponEntry.js
@@ -3,17 +3,17 @@ import React, { useState } from 'react';
 const CouponEntry = () => {
   const [formState, setFormState] = useState(
     { 
-      couponName: '',
-      productName: '',
+      couponTitle: '',
+      product: '',
       vendor: '',
-      discountValue: '',
+      amountOff: '',
       currency: '',
-      expDate: '',
-      maxRedeem: '',
+      redeemBy: '',
+      maxRedemptions: '',
     }
   );
 
-  const { couponName, productName, vendor, discountValue, currency, expDate, maxRedeem } = formState
+  const { couponTitle, product, vendor, amountOff, currency, redeemBy, maxRedemptions } = formState
 
   function handleChange(e) {
     setFormState({...formState, [e.target.name]: e.target.value})
@@ -30,33 +30,33 @@ const CouponEntry = () => {
         <h4 className='left-align bold-text'>Create a new coupon</h4>
         <section className='green-bg form-section'>          
           <form id='coupon-entry' onSubmit={handleSubmit} className='form'>
-            <label htmlFor='couponName' className='blue-bg row'>
+            <label htmlFor='couponTitle' className='blue-bg row'>
               <p className='col-4'>Name:</p>
-              <input type="text" name='couponName' defaultValue={couponName} onChange={handleChange} className='col-8'/>
+              <input type="text" name='couponTitle' defaultValue={couponTitle} onChange={handleChange} className='col-8'/>
             </label><br/>
-            <label htmlFor='productName' className='blue-bg row'>
+            <label htmlFor='product' className='blue-bg row'>
               <p className='col-4'>Product:</p>
-              <input type="text" name='productName' defaultValue={productName} onChange={handleChange} className='col-8'/>
+              <input type="text" name='product' defaultValue={product} onChange={handleChange} className='col-8'/>
             </label><br/>
             <label htmlFor='vendor' className='blue-bg row'>
               <p className='col-4'>Vendor / Manufacturer:</p>
               <input type="text" name='vendor' defaultValue={vendor} onChange={handleChange} className='col-8'/>
             </label><br/>
-            <label htmlFor='discountValue' className='blue-bg row'>
+            <label htmlFor='amountOff' className='blue-bg row'>
               <p className='col-4'>Discount Value (% or $ off):</p>
-              <input type="text" name='discountValue' defaultValue={discountValue} onChange={handleChange} className='col-8'/>
+              <input type="text" name='amountOff' defaultValue={amountOff} onChange={handleChange} className='col-8'/>
             </label><br/>
             <label htmlFor='currency' className='blue-bg row'>
               <p className='col-4'>Currency:</p>
               <input type="text" name='currency' defaultValue={currency} onChange={handleChange} className='col-8'/>
             </label><br/>
-            <label htmlFor='expDate' className='blue-bg row'>
+            <label htmlFor='redeemBy' className='blue-bg row'>
               <p className='col-4'>Expiration Date:</p>
-              <input type="text" name='expDate' defaultValue={expDate} onChange={handleChange} className='col-8'/>
+              <input type="text" name='redeemBy' defaultValue={redeemBy} onChange={handleChange} className='col-8'/>
             </label><br/>
-            <label htmlFor='maxRedeem' className='blue-bg row'>
+            <label htmlFor='maxRedemptions' className='blue-bg row'>
               <p className='col-4'>Maximum Redemptions:</p>
-              <input type="text" name='maxRedeem' defaultValue={maxRedeem} onChange={handleChange} className='col-8'/>
+              <input type="text" name='maxRedemptions' defaultValue={maxRedemptions} onChange={handleChange} className='col-8'/>
             </label><br/>
             <button className='purple-bg'>Submit</button>
           </form>
